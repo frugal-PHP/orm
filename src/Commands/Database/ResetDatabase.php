57ee5f018a7572e485c77370a09c6ff0556329a1
Grégory COLLIN <gregory@badass-coding.com>
2025-07-24 10:50:10 +0200

    Simplification de la commande et bugfix

--- src/Commands/Database/ResetDatabase.php
+++ src/Commands/Database/ResetDatabase.php
@@ -2,44 +2,36 @@
 
 namespace FrugalPhpPlugin\Orm\Commands\Database;
 
-use FrugalPhpPlugin\Orm\Services\SqliteDatabase;
-use React\EventLoop\Loop;
+use Frugal\Core\Services\FrugalContainer;
 use function React\Async\await;
 
 class ResetDatabase
 {
     public static function run()
     {
-        $loop = Loop::get();
-
-        $databaseFilePathName = getenv('ROOT_DIR') . "/database/bdd.sqlite";
+        $databaseFilePathName = getenv('ROOT_DIR') . "/database/".getenv('DATABASE_SQLITE_FILENAME');
         if (file_exists($databaseFilePathName)) {
             unlink($databaseFilePathName);
         }
 
-        $sql = file_get_contents(getenv('ROOT_DIR') . "/database/bdd.sql");
+        $sql = file_get_contents(getenv('ROOT_DIR') . "/database/".getenv('DATABASE_SQL_FILENAME'));
         $queries = array_filter(array_map('trim', explode(';', $sql)));
 
-        $loop->futureTick(function () use ($queries) {
-            \React\Async\async(function () use ($queries) {
-                await(SqliteDatabase::execute("PRAGMA journal_mode = WAL;"));
-                foreach ($queries as $query) {
-                    if ($query === '') continue;
+        $orm = FrugalContainer::getInstance()->get('orm');
+        
+        await($orm->execute("PRAGMA journal_mode = WAL;"));
+        foreach ($queries as $query) {
+            if ($query === '') continue;
 
-                    try {
-                        await(SqliteDatabase::execute($query));
-                    } catch (\Throwable $e) {
-                        echo "Erreur SQL : " . $e->getMessage() . "\n";
-                        echo "Requête : " . $query . "\n";
-                        exit(1);
-                    }
-                }
+            try {
+                await($orm->execute($query));
+            } catch (\Throwable $e) {
+                echo "Erreur SQL : " . $e->getMessage() . "\n";
+                echo "Requête : " . $query . "\n";
+            }
+        }
 
-                Loop::stop();
-            })();
-        });
-
-        $loop->run();
+        $orm->close();
 
         return 0;
     }
